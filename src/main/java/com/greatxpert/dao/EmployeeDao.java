a8079035abcbf6d775e2769ca0f02a970cefe2b6
Suresh S <sureshsk@gmail.com>
2016-09-10 10:33:00 -0700

    Initial commit

---
+++ src/main/java/com/greatxpert/dao/EmployeeDao.java
@@ -0,0 +1,19 @@
+package com.greatxpert.dao;
+
+import com.greatxpert.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeDao {
+
+    Employee findById(int id);
+
+    void saveEmployee(Employee employee);
+
+    void deleteEmployeeBySsn(String ssn);
+
+    List<Employee> findAllEmployees();
+
+    Employee findEmployeeBySsn(String ssn);
+
+}
